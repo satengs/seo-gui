a25328b202cb0c7a9f8bdbd559283ba5fd554e2c
SedGrigoryan <sedgrigoryan20@gmail.com>
2025-05-03 23:16:56 +0400

    Fix keywords issue

--- components/shared/AccountInfoItem/index.tsx
+++ components/shared/AccountInfoItem/index.tsx
@@ -1,52 +1,37 @@
 import React from 'react';
-import { Card } from '@/components/ui/card';
-import { IAccount } from '@/types';
 import { format } from 'date-fns';
+import { Card } from '@/components/ui/card';
+import CustomProgressBar from '@/components/shared/CustomProgressBar';
+import { IAccount } from '@/types';
 
 interface IAccountInfoItem {
   account: IAccount;
 }
 
 const AccountInfoItem: React.FC<IAccountInfoItem> = ({ account }) => {
-  // const { toast } = useToast();
-  // const router = useRouter();
-
-  // const handleLogOut = useCallback(async () => {
-  //   try {
-  //     await axiosClient.get('/api/auth/log-out');
-  //     router.replace('/auth/sign-in');
-  //     toast({
-  //       title: 'Success',
-  //       description: 'User successfully log out',
-  //     });
-  //   } catch (err) {
-  //     toast({
-  //       title: 'Failed',
-  //       description: 'Log out failed ',
-  //       variant: 'destructive',
-  //     });
-  //   }
-  // }, [toast]);
-
   return (
     <Card className="py-2 border-0">
       <div className="flex items-center justify-between gap-4">
         {account?.lastSearchDate ? (
-          <p className="text-sm font-medium text-muted-foreground border-x  px-4">
+          <p className="text-sm font-medium text-muted-foreground border-x px-4">
             Last searched at:{' '}
             {format(account?.lastSearchDate, 'yyyy-MM-dd HH:mm')}
           </p>
         ) : null}
 
-        <p className="text-sm font-medium text-muted-foreground">
-          {account?.plan_searches_left} / {account?.searches_per_month} searches
-        </p>
+        <CustomProgressBar
+          total={account?.searches_per_month}
+          remaining={account?.plan_searches_left}
+        />
+        {/*<p className="text-sm font-medium text-muted-foreground">*/}
+        {/*  {account?.plan_searches_left} / {account?.searches_per_month} searches*/}
+        {/*</p>*/}
 
         {/*<Button variant={'ghost'} className={'px-0'} onClick={handleLogOut}>*/}
         {/*  <LogOut width={20} height={20} />*/}
         {/*</Button>*/}
 
-        <div className="h-10 w-10 rounded-full bg-primary/10 flex items-center justify-center">
+        <div className="h-10 w-20 rounded-full bg-primary/10 flex items-center justify-center">
           <span>{account?.account_email?.charAt(0).toUpperCase()}</span>
         </div>
       </div>
---
+++ components/shared/CustomProgressBar/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+interface ProgressBarProps {
+  total: number;
+  remaining: number;
+}
+
+const CustomProgressBar: React.FC<ProgressBarProps> = ({
+  total,
+  remaining,
+}) => {
+  const completed = total - remaining;
+  const completedPercentage = (completed / total) * 100;
+  const remainingPercentage = (remaining / total) * 100;
+
+  return (
+    <div className="w-full">
+      <div className="flex justify-between mb-2">
+        <span className="text-sm font-medium text-muted-foreground">
+          {completed.toLocaleString()} / {total.toLocaleString()}
+        </span>
+      </div>
+
+      <div className="h-3 bg-gray-200 rounded-full overflow-hidden shadow-inner">
+        <div className="flex h-full">
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: `${completedPercentage}%` }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+            className="bg-red-500 dark:bg-red-900 h-full"
+          />
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: `${remainingPercentage}%` }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+            className="bg-green-500 dark:bg-green-900 h-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomProgressBar;
